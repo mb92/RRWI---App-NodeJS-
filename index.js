74390e745c0e6096e97d7e11ba3f6e951516c14f
maciek <borowiec@devanddeliver.com>
2017-09-01 15:01:29 +0200

    Uncomment pronsole module

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ var port = process.env.PORT || 3000;        // set our port
 var command_output = ''; //this will contain output of commands that we're run with runCommand fuction...
 var files = []; //array of files uploaded
 
-//we need to just to interface with pronsole.py that works perfectly !
-// var spawn    = require('child_process').spawn;
+we need to just to interface with pronsole.py that works perfectly !
+var spawn    = require('child_process').spawn;
 
-// ///home/pi/printrun/pronsole.py
-// //var pronsole = spawn('python', ['/home/pi/printrun/test2.py','']);
-// var pronsole = spawn('python', ['/home/pi/printrun/pronsole.py','']);
-// //*************************
+///home/pi/printrun/pronsole.py
+//var pronsole = spawn('python', ['/home/pi/printrun/test2.py','']);
+var pronsole = spawn('python', ['/home/pi/printrun/pronsole.py','']);
+//*************************
 
 // **** Routes for api ****
 // =============================================================================
@@ -107,40 +107,40 @@ app.use('/sdprint', sdprintRoute);
 // register routes - all of our routes will be prefixed with /api
 
 // **** Pronsole connection ****
-// console.log('pronsole.py is spawned, waiting 3 seconds and sending connect...');
-// setTimeout( function(){
-//   //calling connect without params here (todo add ttyUSB etc, but hey the defaults work just fine now ;)
-//   pronsole.stdin.write('connect\n');
-// }, 3000 );
+console.log('pronsole.py is spawned, waiting 3 seconds and sending connect...');
+setTimeout( function(){
+  //calling connect without params here (todo add ttyUSB etc, but hey the defaults work just fine now ;)
+  pronsole.stdin.write('connect\n');
+}, 3000 );
 
-// //console.log('pronsole.py is spawned, waiting 3 seconds and sending monitor...');
-// //setTimeout( function(){
-//   //calling connect without params here (todo add ttyUSB etc, but hey the defaults work just fine now ;)
-// //  pronsole.stdin.write('monitor\n'); //cool this just works like we want -> need some ajax though to feed it back to the browser...
-// //}, 3000 );
+//console.log('pronsole.py is spawned, waiting 3 seconds and sending monitor...');
+//setTimeout( function(){
+  //calling connect without params here (todo add ttyUSB etc, but hey the defaults work just fine now ;)
+//  pronsole.stdin.write('monitor\n'); //cool this just works like we want -> need some ajax though to feed it back to the browser...
+//}, 3000 );
 
 
 
-// pronsole.stdout.on('data', function (data) {
-//   console.log( 'pronsole: '+data ); //todo use some ajax to feed it to our browser here...
-// });
+pronsole.stdout.on('data', function (data) {
+  console.log( 'pronsole: '+data ); //todo use some ajax to feed it to our browser here...
+});
 
-// pronsole.stderr.on('data', function (data) {
-//   console.log('pronsole err: ' + data);
-// });
+pronsole.stderr.on('data', function (data) {
+  console.log('pronsole err: ' + data);
+});
 
-// pronsole.stdout.on('end', function(data) {
-//   pronsole.stdout.end();
-// } );
+pronsole.stdout.on('end', function(data) {
+  pronsole.stdout.end();
+} );
 
-// pronsole.on('exit', function (code) {
-//   if (code !== 0) {
-//     console.log('pronsole process exited with code ' + code);
-//   }
-//   console.log('pronsole exited!');
-//   pronsole.stdin.end(); 
-//   //todo just respawn pronsole here!!!
-// });
+pronsole.on('exit', function (code) {
+  if (code !== 0) {
+    console.log('pronsole process exited with code ' + code);
+  }
+  console.log('pronsole exited!');
+  pronsole.stdin.end(); 
+  //todo just respawn pronsole here!!!
+});
 
 
 // START THE SERVER
